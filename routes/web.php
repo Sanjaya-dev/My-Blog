df6287bfe6ca62cd2d22f7dd818fb0eefd1353c8
Fernanda <sanjaya.dev20@gmail.com>
2024-02-26 20:37:27 +0700

    membuat route user dan article pada dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminArticleController;
+use App\Http\Controllers\Dashboard\AdminUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +24,28 @@ Route::get('/home', function(){
     return view('home');
 })->name('home');
 
-// socialite google
-Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
-Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
+Route::middleware(['auth'])->group(function () {
+    // socialite google
+    Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+    Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
+    
+    // admin
+    Route::get('/admin/dashboard',function(){
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    Route::get('/admin/dashboard/article',[AdminArticleController::class, 'index'])->name('admin.dashboard.article');
+    Route::get('/admin/dashboard/article/create',[AdminArticleController::class, 'create'])->name('admin.dashboard.article.create');
+    Route::post('/admin/dashboard/article',[AdminArticleController::class, 'store'])->name('admin.dashboard.article.store');
+    Route::get('/admin/dashboard/user',[AdminUserController::class, 'index'])->name('admin.dashboard.user');
+    
+    // user
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
+
 
 require __DIR__.'/auth.php';
