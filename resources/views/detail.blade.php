6a4b97cfb87793c5699f20540479f47f3dfd85b9
Fernanda <sanjaya.dev20@gmail.com>
2024-03-14 18:07:04 +0700

    memperbaiki tampilan detail article

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -8,12 +8,16 @@ Detail article
 
 @section('content')
 <div class="container">
-    <div class="card">
-        <img src="{{asset('storage/thumbnails/'.$article->photo)}}" class="card-img-top" alt="...">
-        <div class="card-body">
+    <div class="card mt-5">
+        <div class="card-header text-center bg-success">
             <h5 class="card-title">{{$article->title}}</h5>
-            <p class="card-text">{{$article->content}}</p>
-            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <p>Author : {{$article->User->name}} &#8739; Upload : {{date_format($article->created_at,"d/m/Y H:i:s")}}
+            </p>
+        </div>
+        <div class="card-body">
+            <p class="card-text">{!! nl2br($article->content)!!}</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated
+                    {{date_format($article->updated_at,"d/m/Y H:i:s")}}</small></p>
         </div>
     </div>
 </div>
