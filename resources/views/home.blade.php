b246631bc18d3fa16809cfeaa8c4fe7acb4fc1ab
Fernanda <sanjaya.dev20@gmail.com>
2024-03-14 18:07:41 +0700

    menghilangkan image pada article pada setiap card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@ article
     <div class="container">
         <div id="search-article" class="row">
             <div class="search-blog col-lg-4 text-center pt-3 m-auto">
-                <h1>Article</h1>
+                <h1 class="text-success">Selamat datang di website kami</h1>
                 <form action="#" method="get">
                     <div class="input-group">
                         <input type="text" placeholder="search blog" class="form-control form-control-sm">
@@ -26,11 +26,12 @@ article
             @foreach ($articles as $article )
             <div class="col-lg-4 pb-3">
                 <div class="card" style="width: 18rem;">
-                    <img src="{{asset('storage/thumbnails/'.$article->photo)}}" class="card-img-top img-thumbnail"
-                        style="max-width: 290px;max-height: 200px">
+                    {{-- <img src="{{asset('storage/thumbnails/'.$article->photo)}}" class="card-img-top img-thumbnail"
+                        style="max-width: 290px;max-height: 200px"> --}}
                     <div class="card-body">
-                        <h5 class="card-title">{{$article->title}}</h5>
-                        <a href="{{route('home.detailarticle',$article->id)}}" class="btn btn-primary">Read More</a>
+                        <p class="card-title">{{$article->title}}</p>
+                        <a href="{{route('home.detailarticle',$article->id)}}" class="btn btn-primary btn-sm">Read
+                            More</a>
                     </div>
                 </div>
             </div>
